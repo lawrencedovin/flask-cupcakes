a9e53680521f224d71f3aa4e81b433aad75eb1ad
Lawrence Dovin <lawrencedovin@gmail.com>
2020-12-21 16:13:41 -0500

    Added the Cupcake Model

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, request, jsonify
+from models import *
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SECRET_KEY'] = 'cluckcluck'
+
+connect_db(app)
